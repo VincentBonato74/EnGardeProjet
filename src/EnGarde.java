dfb9b11485e757dda737beb5948c070bdd5427a5
VincentBonato74 <bonato.vincent74@gmail.com>
2021-05-18 10:48:41 +0200

    Modif class En Garde

--- src/EnGarde.java
+++ src/EnGarde.java
@@ -1,3 +1,18 @@
+import Modele.*;
+import Controller.ControllerMediateur;
+import Vue.InterfaceGraphique;
+
 public class EnGarde {
+
+    public static void main(String [] args) {
+        try {
+            Jeu j = new Jeu();
+            Manche m = new Manche();
+            ControllerMediateur c = new ControllerMediateur(j);
+            InterfaceGraphique.demarrer(j, c);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
